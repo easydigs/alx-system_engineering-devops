93a190d554c0d01531a774b7d7c93c84c1540e25
easydigs <easydigitalkoncept@gmail.com>
2024-06-12 16:41:24 +0100

    User limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,12 +3,12 @@
 
 # increase hard file limit for holberton user.
 exec {'increase-hard-file-limit-forholberton-user':
-  command => 'sed -i "/^holberton hard/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/:/bin/'
+  command => 'sed -i "s/holberton soft/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
 }
 
 # increase soft file limit for holberton user.
-exec {'increase-soft-file-limit-forholberton-user':
-  command => 'sed -i "/^holberton soft/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/:/bin/'
+exec {'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "s/holberton soft/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
 }
