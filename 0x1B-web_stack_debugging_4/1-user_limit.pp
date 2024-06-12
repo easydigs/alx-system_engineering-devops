f04dc3f52eace160486ca8ecb3f41209991318fe
easydigs <easydigitalkoncept@gmail.com>
2024-06-12 16:27:31 +0100

    User limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,14 @@
 # Change the OS configuration so that it is possible to login with the
 # holberton user and open a file without any error message.
 
-exec {'OS security config':
-  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
-  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+# increase hard file limit for holberton user.
+exec {'increase-hard-file-limit-forholberton-user':
+  command => 'sed -i "/^holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/:/bin/'
+}
+
+# increase soft file limit for holberton user.
+exec {'increase-soft-file-limit-forholberton-user':
+  command => 'sed -i "/^holberton hard/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/:/bin/'
 }
